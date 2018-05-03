12aa8777447db941eeacc2e42ba1aa3c255c0748
Vojtech Huser <vojtech.huser@gmail.com>
2018-05-03 16:05:18 -0400

    Update AchillesHeel_v5.sql
    missing underscores

--- inst/sql/sql_server/AchillesHeel_v5.sql
+++ inst/sql/sql_server/AchillesHeel_v5.sql
@@ -139,10 +139,10 @@ insert into @results_database_schema.ACHILLES_results_derived (statistic_value,m
 insert into @results_database_schema.ACHILLES_results_derived (statistic_value,measure_id)    
   select 
    cast(
-    100.0*count_value/(select count_value as total_pts from @resultsDatabaseSchema.achilles_results r where analysis_id = 1) 
+    100.0*count_value/(select count_value as total_pts from @results_database_schema.achilles_results r where analysis_id = 1) 
     as FLOAT) as statistic_value,
    cast(concat('ach_',cast(analysis_id as VARCHAR),':Percentage') as VARCHAR(255)) as measure_id
-from @resultsDatabaseSchema.ACHILLES_results 
+from @results_database_schema.ACHILLES_results 
 where analysis_id in (2000,2001,2002,2003);
 
 
